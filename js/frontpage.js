b119db7e4f98a446625df625e44a53e7e7d77201
LioFires <spills.erratic.0d@icloud.com>
2022-10-26 19:56:59 +0200

    Updating

--- js/frontpage.js
+++ js/frontpage.js
@@ -7,39 +7,3 @@ const change_theme_emoji = () => change_theme_button.innerText = (change_theme_b
     The when_input_changes function is gonnna be moved to the C++ file.
     and be redone completly.
 */
-// function when_input_changes()
-// {
-//     let str;
-//     for (const i of check_box_list.children)
-//     {
-//         if (i.firstChild.checked)
-//         {
-//             switch (i.firstChild.id)
-//             {
-//                 case "reverse-text-checked":
-//                     str = Module.reverse_string(input_felt.value);
-//                     input_felt.value = str;
-//                 break;
-    
-//                 case "uppercase-text-checked":
-//                 case "lowercase-text-checked":
-//                     str = Module.lower_or_uppercase_string(input_felt.value);
-//                     input_felt.value = str;
-//                 break;
-    
-//                 case "color-text-checked":
-
-//                 break;
-    
-//                 case "download-text-checked":
-    
-//                 break;
-    
-//                 case "copy-text-checked":
-    
-//                 break;
-            
-//             }
-//         }
-//     }
-// }
