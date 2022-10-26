9142f4abe81bef16173ae5c067d91dc37489ff54
LioFires <spills.erratic.0d@icloud.com>
2022-10-26 19:38:05 +0200

    Updating

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,14 +1,8 @@
 const input_felt = document.getElementById("text-input");
-const check_box_list = document.getElementById("manipulate-text-list");
+const change_theme_button = document.getElementById("change-theme-button");
 
-input_felt.addEventListener("keydown", (event) => 
-{
-        if (event.key === "Enter")
-        {
-            document.getElementById("output-text").innerHTML = input_felt.value;
-        }
-});
-
+// change the emoji of the theme button to the correct one.
+const change_theme_emoji = () => change_theme_button.innerText = (change_theme_button.innerText === "🌑") ? "🌕" :  "🌑" || (change_theme_button.innerText === "🌕") ? "🌑" : "🌕";
 /*
     The when_input_changes function is gonnna be moved to the C++ file.
     and be redone completly.
